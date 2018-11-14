68cd81a86dcc0f58a474ec1f66604800f7dce423
Chrissie Williams <chrissie_annisa_williams@hotmail.com>
2018-11-14 05:34:05 +0000

    Exercise #4 Completed

--- app.js
+++ app.js
@@ -2,19 +2,30 @@
 window.onload = function() {
     
     var submitBtn = document.getElementById("submitBtn");
+    var getAllDefinitionsBtn = document.getElementById("getAllDefinitions");
     var  httpRequest;
-
+    httpRequest = new XMLHttpRequest();
+    
 submitBtn.addEventListener('click', function(e) {
     
     e.preventDefault();
     var formdata = document.getElementById("search-term").value;
-    httpRequest = new XMLHttpRequest();
-    
+
     //GET Request
-    var url = "request.php?q=" + "" + formdata;
+    //var url = "searchterm.php?q=definition"; /*To get only definition*/
+    var url = "searchterm.php?q=" + "" + formdata;
     httpRequest.onreadystatechange = loadDefinition;
     httpRequest.open('GET', url);
     httpRequest.send();
+});
+
+getAllDefinitionsBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    var url = "request.php?q=&all=true";
+    httpRequest.onreadystatechange = loadAllDefinition;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+    
 });
 
 function loadDefinition()
@@ -23,8 +34,47 @@ function loadDefinition()
     {
         if (httpRequest.status === 200)
         {
-            var response = httpRequest.responseText;
-            document.getElementById("result").innerHTML = "" + response;
+            var response = httpRequest.responseText; //Use .responseText
+            //alert(response); //Response is display using alert
+            document.getElementById("results").innerHTML = "" + response;
+        }
+        else 
+        {
+            alert('There was a problem with the request.');
+        }
+    }
+}
+
+function loadAllDefinition()
+{
+    if (httpRequest.readyState === XMLHttpRequest.DONE)
+    {
+        if (httpRequest.status === 200)
+        {
+            var response = httpRequest.responseXML; //With XML, use .responseXML with XML data, not .responseText
+            var results = document.querySelector('#results');
+            var definitions = response.getElementsByTagName("definition");
+            //console.log(definitions);
+            var orderedlist = document.createElement("ol");
+            results.appendChild(orderedlist);
+            for (var i = 0; i < definitions.length; i++) {
+                var listItem = document.createElement('li');
+                var dtHeading = document.createElement('h3');
+                var headingText = document.createTextNode(definitions[i].getAttribute("name"));
+                dtHeading.appendChild(headingText);
+                listItem.appendChild(dtHeading);
+                var dtText = document.createElement("p");
+                var definitionText = document.createTextNode(definitions[i].childNodes[0].nodeValue);
+                dtText.appendChild(definitionText);
+                listItem.appendChild(dtText);
+                var author = document.createElement("p");
+                var authorText = definitions[i].getAttribute("author");
+                var authorTxt = document.createTextNode("-" + authorText);
+                author.appendChild(authorTxt);
+                listItem.appendChild(author);
+                orderedlist.appendChild(listItem);
+            }
+            
         }
         else 
         {
--- request.php
+++ request.php
@@ -1,5 +1,5 @@
 <?php
-
+/* //Uncomment when you want to search for one term
 // accept a term (keyword)
 // respond with a value
 
@@ -15,6 +15,35 @@ $definition = [
 ];
 
 
-
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+*/ //Uncomment when you want to search for one term
+
+$myXMLData = '<?xml version = "1.0" encoding ="UTF-8"?>
+<entries>
+    <definition name = "definition" author = "John">
+    a statement of the exact meaning of a word, especially in a dictionary. 
+    </definition>
+    <definition name = "bar" author ="Mary">
+    a place that sells alcoholic beverages
+    </definition>
+    <definition name ="ajax" author = "Kimberly">
+    technique which involves the use of javascript and xml
+    </definition>
+    <definition name = "html" author = "Michelle">
+    The standard markup language for creating web pages and web applications
+    </definition>
+    <definition name = "css" author = "Charlene">
+    A style sheet language used for describing the presentation of a document written in a markup language
+    </definition>
+    <definition name = "javascript" author = "Audrea">
+    A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name = "php" author = "Chris-San">
+    A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+</entries>'; 
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($myXMLData);
+echo $xmlOutput->asXML();
